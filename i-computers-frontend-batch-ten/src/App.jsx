1f8bbf9040e7063881ad56834fa7fb90685c36a1
pasindu <pasindunawagamuwa4@gmail.com>
2026-08-25 16:55:00 +0530

    React router dom

--- i-computers-frontend-batch-ten/src/App.jsx
+++ i-computers-frontend-batch-ten/src/App.jsx
@@ -1,28 +1,21 @@
-import './App.css'
-import TrendingProduct from './components/trendingProducts'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import AdminPage from "./pages/adminPage";
+import HomePage from "./pages/homePage";
+// import TrendingProduct from './components/trendingProducts'
 // import ProductCard from './components/productCard'
 
 function App() {
-  
-
   return (
     <>
-     <div>
-      
-      <TrendingProduct/>
-
-      <div>
-        <h1>Mac book air</h1>
-        <img src="https://picsum.photos/id/1/200/300" alt="Random image" />
-        <p>LKR 150,000/-</p>
-        <button>Buy now</button>
+      <div className="w-full h-screen border-[6px] flex justify-center items-center">
+        <Routes>
+          <Route path="/" element={<HomePage/>}/>
+          <Route path="/admin" element={<AdminPage/>}/>
+        </Routes>
       </div>
-
-      
-
-     </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
